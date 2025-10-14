5dd2d1dfab9dd8672bef6e8b602c369b4b9979d2
bendaone34 <benny102811@gmail.com>
2025-10-14 17:24:43 +0100

    commit1

--- Math.java
+++ Math.java
@@ -1,4 +1,12 @@
-public class Math():
+public class Math {
+    public static int add(int num1, int num2) {
+        num1 = 2;
+        num2 = 4;
+       int total = num1 + num2;
+        return total;
+    }
+
+}
     
     
 
